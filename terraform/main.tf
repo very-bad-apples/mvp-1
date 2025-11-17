cd180af5fc0efef2cabe0cf8d78096b359482484
Kenneth Yu <966806+kennyu@users.noreply.github.com>
2025-11-17 16:38:35 -0600

    chore: Terraform cleanup

--- terraform/main.tf
+++ terraform/main.tf
@@ -79,6 +79,8 @@ resource "aws_s3_bucket_lifecycle_configuration" "video_storage" {
     id     = "delete-old-versions"
     status = "Enabled"
 
+    filter {}
+
     noncurrent_version_expiration {
       noncurrent_days = 30
     }
@@ -87,6 +89,8 @@ resource "aws_s3_bucket_lifecycle_configuration" "video_storage" {
   rule {
     id     = "cleanup-incomplete-uploads"
     status = "Enabled"
+
+    filter {}
 
     abort_incomplete_multipart_upload {
       days_after_initiation = 7
